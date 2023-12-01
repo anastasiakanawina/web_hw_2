706389de303e41ac071adb7d330797a104feeace
avkanavina <a.kanavina@skyeng.ru>
2023-12-02 01:48:44 +0500

    edit models

--- catalog/models.py
+++ catalog/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -14,3 +17,6 @@ class Product(models.Model):
     price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
