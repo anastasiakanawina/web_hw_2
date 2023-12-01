436a1992a12eee011da85f0a12bf6e027315bcf8
avkanavina <a.kanavina@skyeng.ru>
2023-12-02 00:34:34 +0500

    add models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_per_unit', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+
+admin.site.register(Product, ProductAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=None)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image_preview = models.ImageField(upload_to='product_images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
