1243fd6676d4d465c4a2d03860338c6e6f4b106a
avkanavina <a.kanavina@skyeng.ru>
2023-12-02 00:37:45 +0500

    delete last migration 0001

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    created_at = models.DateTimeField(default=None)
 
 
 class Product(models.Model):
